92fd5e1922807bfadf93f2810bf87408c9f13faa
tahsindogrul <tahsindogrul@icloud.com>
2024-05-31 20:42:40 +0300

    Add project files.

---
+++ EgitimTakip.Models/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Models
+{
+    public class AppUser:BaseModel
+    {
+        public string UserName { get; set; }    
+        public string Password { get; set; }
+
+        public virtual ICollection<Company> Companies { get; set; }= new
+            List<Company>();
+
+    }
+}
