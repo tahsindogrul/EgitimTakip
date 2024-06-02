b3479b08f0c8e3badbb0a27cfb4553bda2873e4e
tahsindogrul <tahsindogrul@icloud.com>
2024-06-02 21:04:46 +0300

    user type controller oluşturuldu

---
+++ EgitimTakip.Web/Controllers/UserTypeController.cs
@@ -0,0 +1,61 @@
+﻿using EgitimTakip.Data;
+using EgitimTakip.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgitimTakip.Web.Controllers
+{
+    public class UserTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _context.UserTypes.Where(ut => !ut.IsDeleted) });
+        }
+
+        [HttpPost]
+        public IActionResult Add(AppUserType userType)
+        {
+            _context.UserTypes.Add(userType);
+            _context.SaveChanges();
+            return Ok(userType);
+        }
+
+        [HttpPost]
+        public IActionResult Update(AppUserType userType)
+        {
+            _context.UserTypes.Update(userType);
+            _context.SaveChanges();
+                return Ok(userType);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var userType= _context.UserTypes.Find(id);
+            userType.IsDeleted = true;
+            _context.UserTypes.Update(userType);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_context.UserTypes.Find(id));
+        }
+
+
+
+    }
+}
