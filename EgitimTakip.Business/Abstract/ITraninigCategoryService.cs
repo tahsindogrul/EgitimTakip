42b0da3b03e5a6fce47fbb454e9b48e8a687b569
tahsindogrul <tahsindogrul@icloud.com>
2024-07-04 21:21:05 +0300

    Business katmanına Abstract ve Concrete kısımları oluşturulup içerisine servisler yazıldı.

---
+++ EgitimTakip.Business/Abstract/ITraninigCategoryService.cs
@@ -0,0 +1,21 @@
+﻿using EgitimTakip.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimTakip.Business.Abstract
+{
+    public interface ITraninigCategoryService
+    {
+        IQueryable<TrainingCategory> GetAll();
+        TrainingCategory Add(TrainingCategory category);
+        bool Delete(int categoryId);
+        TrainingCategory Update(TrainingCategory category);
+        TrainingCategory GetById(int categoryId);
+
+
+    }
+}
